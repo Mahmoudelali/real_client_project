9584798411f9ae2879e76b873cf2cc77dcba87de
mahmoudElali <mahmud.elali98@gmail.com>
2023-04-13 04:25:05 +0300

    added missing import

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,81 +2,82 @@ import Order from '../models/orderModel.js';
 
 // GET /api/orders
 export const getAllOrders = async (req, res) => {
-  try {
-    const orders = await Order.find();
-    res.status(200).json(orders);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+	try {
+		const orders = await Order.find();
+		res.status(200).json(orders);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 };
 
 // POST /api/orders
 export const createOrder = async (req, res) => {
-  const { user_id, product_id, state, date, address, message, total } = req.body;
-  const order = new Order({
-    user_id,
-    product_id,
-    state,
-    date,
-    address,
-    message,
-    total
-  });
+	const { user_id, product_id, state, date, address, message, total } =
+		req.body;
+	const order = new Order({
+		user_id,
+		product_id,
+		state,
+		date,
+		address,
+		message,
+		total,
+	});
 
-  try {
-    const newOrder = await order.save();
-    res.status(201).json(newOrder);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+	try {
+		const newOrder = await order.save();
+		res.status(201).json(newOrder);
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
 };
 
 // GET /api/orders/:orderId
 export const getOrderById = async (req, res) => {
-  try {
-    const order = await Order.findById(req.params.orderId);
-    if (!order) {
-      return res.status(404).json({ message: 'Order not found' });
-    }
-    res.status(200).json(order);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+	try {
+		const order = await Order.findById(req.params.orderId);
+		if (!order) {
+			return res.status(404).json({ message: 'Order not found' });
+		}
+		res.status(200).json(order);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 };
 
 // PUT /api/orders/:orderId
 export const updateOrder = async (req, res) => {
-  try {
-    const order = await Order.findById(req.params.orderId);
-    if (!order) {
-      return res.status(404).json({ message: 'Order not found' });
-    }
+	try {
+		const order = await Order.findById(req.params.orderId);
+		if (!order) {
+			return res.status(404).json({ message: 'Order not found' });
+		}
 
-    order.user_id = req.body.user_id || order.user_id;
-    order.product_id = req.body.product_id || order.product_id;
-    order.state = req.body.state || order.state;
-    order.date = req.body.date || order.date;
-    order.address = req.body.address || order.address;
-    order.message = req.body.message || order.message;
-    order.total = req.body.total || order.total;
+		order.user_id = req.body.user_id || order.user_id;
+		order.product_id = req.body.product_id || order.product_id;
+		order.state = req.body.state || order.state;
+		order.date = req.body.date || order.date;
+		order.address = req.body.address || order.address;
+		order.message = req.body.message || order.message;
+		order.total = req.body.total || order.total;
 
-    const updatedOrder = await order.save();
-    res.status(200).json(updatedOrder);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+		const updatedOrder = await order.save();
+		res.status(200).json(updatedOrder);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 };
 
 // DELETE /api/orders/:orderId
 export const deleteOrder = async (req, res) => {
-  try {
-    const order = await Order.findById(req.params.orderId);
-    if (!order) {
-      return res.status(404).json({ message: 'Order not found' });
-    }
-    await order.remove();
-    res.status(200).json({ message: 'Order deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+	try {
+		const order = await Order.findById(req.params.orderId);
+		if (!order) {
+			return res.status(404).json({ message: 'Order not found' });
+		}
+		await order.remove();
+		res.status(200).json({ message: 'Order deleted successfully' });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 };
