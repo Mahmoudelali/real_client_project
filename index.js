42e7607b1b83c0ed37828ee65c9b3b94020b5cb3
Emile Kanaan <emilekanaan99@gmail.com>
2023-04-15 08:36:33 +0300

    Emile Kanaan - resolving conflicts

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import connectToDatabase from './dataBase/dataBase.js';
-import userRouter from './routes/userRouter.js';
-import orderRouter from './routes/orderRouter.js';
-import social_mediaRouter from './routes/social_mediaRouter.js';
-import profitRouter from './routes/profitRouter.js';
-import categoryRouter from './routes/categoryRouter.js';
-import subCategoryRouter from './routes/sub_categoryRouter.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectToDatabase from "./dataBase/dataBase.js";
+import userRouter from "./routes/userRouter.js";
+import orderRouter from "./routes/orderRouter.js";
+import social_mediaRouter from "./routes/social_mediaRouter.js";
+import profitRouter from "./routes/profitRouter.js";
+import categoryRouter from "./routes/categoryRouter.js";
+import subCategoryRouter from "./routes/sub_categoryRouter.js";
+import productRouter from "./routes/productRouter.js";
+import websiteSettingRouter from "./routes/website_settingRouter.js";
 
 dotenv.config();
 
@@ -21,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 const app = new express();
 
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+	app.use(morgan("dev"));
 }
 
 app.use(express.json());
@@ -32,43 +33,14 @@ app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
-
 app.listen(
 	PORT,
-	console.log(
-		`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`,
-	),
+	console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`)
 );
 
-app.use('/user', userRouter);
-app.use('/order', orderRouter);
-app.use('/products', productRouter);
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
-
-app.listen(
-	PORT,
-	console.log(
-		`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`,
-	),
-);
-
-app.use('/user', userRouter);
-app.use('/order', orderRouter);
-app.use('/products', productRouter);
 app.get("/", (req, res) => {
 	res.send("API is running...");
 });
-
-app.listen(
-  PORT,
-  console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`)
-);
-
 app.use("/user", userRouter);
 app.use("/order", orderRouter);
 app.use("/products", productRouter);
