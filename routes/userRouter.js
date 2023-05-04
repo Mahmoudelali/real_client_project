f1bb6c80bddfed94dfef1d6698993af3d25ae206
mahmoudElali <mahmud.elali98@gmail.com>
2023-05-04 14:52:01 +0300

    mahmoudElali_editRole

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,66 +1,71 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
 import {
-    getAll,
-    getUsers,
-    get,
-    register,
-    login,
-    logout,
-    del,
-    isLoggedIn,
-    addAdmin,
-    addSuperAdmin,
-} from "../controllers/userController.js";
-import testPhone from "../middleware/validatePhone.js";
-import { admin, superAdmin, verifyUser } from "../middleware/auth.js";
-import upload from "../middleware/imagesUpload.js";
+	getAll,
+	getUsers,
+	get,
+	register,
+	login,
+	logout,
+	del,
+	isLoggedIn,
+	addAdmin,
+	addSuperAdmin,
+	editAdminRole,
+} from '../controllers/userController.js';
+import testPhone from '../middleware/validatePhone.js';
+import { admin, superAdmin, verifyUser } from '../middleware/auth.js';
+import upload from '../middleware/imagesUpload.js';
 
 // get users and admin and superAdmins
-router.get("/", verifyUser, admin, superAdmin, getAll);
+router.get('/', verifyUser, admin, superAdmin, getAll);
 
 // get users
-router.get("/users", verifyUser, admin, getUsers);
+router.get('/users', verifyUser, admin, getUsers);
 
 // check if the user is logged in
-router.get("/is-logged-in", isLoggedIn);
+router.get('/is-logged-in', isLoggedIn);
 
 // get user by id
-router.get("/:id", verifyUser, get);
+router.get('/:id', verifyUser, get);
 
 // register a new user account
-router.post("/register", upload, testPhone, register);
+router.post('/register', upload, testPhone, register);
 
 // add a new admin account
 // router.post("/add-admin", verifyUser, admin, superAdmin, testPhone, addAdmin);
 router.post(
-    "/add-admin",
-    verifyUser,
-    admin,
-    superAdmin,
-    upload,
-    testPhone,
-    addAdmin
+	'/add-admin',
+	verifyUser,
+	admin,
+	superAdmin,
+	upload,
+	testPhone,
+	addAdmin,
 );
 
 // add a new super admin account
 router.post(
-    "/add-super-admin",
-    verifyUser,
-    admin,
-    superAdmin,
-    upload,
-    testPhone,
-    addSuperAdmin
+	'/add-super-admin',
+	verifyUser,
+	admin,
+	superAdmin,
+	upload,
+	testPhone,
+	addSuperAdmin,
 );
 
 // login
-router.post("/login", login);
+router.post('/login', login);
 
 // logout
-router.post("/logout", logout);
+router.post('/logout', logout);
 
 // delete a user by id
-router.delete("/:id", verifyUser, admin, superAdmin, del);
+
+router.delete('/:id', verifyUser, admin, superAdmin, del);
+
+//
+router.put('/edit/:id', verifyUser, admin, superAdmin, editAdminRole);
 
 export default router;
