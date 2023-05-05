2d0fe12f8575b2b11f0aa7103f8553ba1509fd10
JanaTartousy <tartousyjana@gmail.com>
2023-05-05 10:30:28 +0300

    adding route for product by categories

--- controllers/productController.js
+++ controllers/productController.js
@@ -58,6 +58,23 @@ class product_controllers {
 			});
 		}
 	}
+	// get all products
+	async getProductByCategory(req, res) {
+		try {
+			const { page, limit } = req.query;
+			const { id}= req.params
+			const options = {
+				page: parseInt(page, 10) || 1,
+				limit: parseInt(limit, 10) || 10,
+			};
+			const products = await ProductModel.paginate({category_id:id}, options);
+			res.status(200).json(products);
+		} catch (error) {
+			res.status(500).json({
+				message: error.message,
+			});
+		}
+	}
 	// get user products
 	async getUserProducts(req, res) {
 		try {
